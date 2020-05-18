b8341893ed2344d45455eb3fd6103219452b54fd
Sakura <875582078@qq.com>
2020-05-18 15:48:56 +0800

    Updates

---
+++ precache-manifest.d8caace8742aa74549374758fa7bbd69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34636e1d2e0b0ab04fb8",
+    "url": "https://SakuraChen297.github.io/blackwhite/css/app.83d2b335.css"
+  },
+  {
+    "revision": "a89eb1f5d7e6f5b6d184",
+    "url": "https://SakuraChen297.github.io/blackwhite/css/chunk-vendors.000e40df.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "https://SakuraChen297.github.io/blackwhite/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "2bdfe18cf5edb07ad1a03d8d2feb3168",
+    "url": "https://SakuraChen297.github.io/blackwhite/index.html"
+  },
+  {
+    "revision": "38801a4bf4c5c7e12b78",
+    "url": "https://SakuraChen297.github.io/blackwhite/js/about.555f9b51.js"
+  },
+  {
+    "revision": "34636e1d2e0b0ab04fb8",
+    "url": "https://SakuraChen297.github.io/blackwhite/js/app.28e7d80f.js"
+  },
+  {
+    "revision": "a89eb1f5d7e6f5b6d184",
+    "url": "https://SakuraChen297.github.io/blackwhite/js/chunk-vendors.007debd6.js"
+  },
+  {
+    "revision": "49edfa9132224543f4fb4ccd16bf8996",
+    "url": "https://SakuraChen297.github.io/blackwhite/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "https://SakuraChen297.github.io/blackwhite/robots.txt"
+  }
+]);
